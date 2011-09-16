187587ba4a56bbd2ce11e1fc289a469657fd8880
Philipp Kretzschmar <philipp.kretzschmar@gmail.com>
2011-09-16 23:42:57 +0200

    working on it

--- src/koerper/DreieckZylinder.java
+++ src/koerper/DreieckZylinder.java
@@ -1,3 +1,68 @@
 package koerper;
-public class DreieckZylinder {
+public class DreieckZylinder extends Form {
+    //Dreieckseiten
+    private int a;
+    private int b;
+    private int c;
+    //Zylinderhoehe
+    private int h;
+
+    @Override
+    protected int berechneGrundflaeche() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected int berechneOberflaeche() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected int berechneUmfangGrundflaeche() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected int berechneVolumen() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected String setzeFormBeschreibung() {
+        return "Dreieckszylinder";
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+
+
 }
--- src/koerper/KreisZylinder.java
+++ src/koerper/KreisZylinder.java
@@ -1,4 +1,46 @@
 package koerper;
-public class KreisZylinder {
+public class KreisZylinder extends Form {
+    private double d;
+    private double h;
 
+    @Override
+    protected int berechneGrundflaeche() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected int berechneOberflaeche() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected int berechneUmfangGrundflaeche() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected int berechneVolumen() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected String setzeFormBeschreibung() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public void setD(double d) {
+        this.d = d;
+    }
+
+    public double getH() {
+        return h;
+    }
+
+    public void setH(double h) {
+        this.h = h;
+    }
 }
